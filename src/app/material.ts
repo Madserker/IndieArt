b8b5834425701eddaf633ec7a2d45ef41a91303a
Sergi Hurtado <madserker@gmail.com>
2018-09-17 15:09:05 +0200

    Not Logged Toolbar Component

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,8 +1,9 @@
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { NgModule } from '../../node_modules/@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule],
-  exports: [MatButtonModule, MatCheckboxModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule],
 })
 export class CustomMaterialModule { }
