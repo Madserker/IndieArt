6cd2d5fb2b78398a84af2d07a113e2ebe981e528
Sergi Hurtado <madserker@gmail.com>
2018-09-19 19:50:18 +0200

    minor fixes and sidenav

--- src/app/material.ts
+++ src/app/material.ts
@@ -2,9 +2,10 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { NgModule } from '../../node_modules/@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatMenuModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatMenuModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatMenuModule, MatSidenavModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatMenuModule, MatSidenavModule],
 })
 export class CustomMaterialModule { }
