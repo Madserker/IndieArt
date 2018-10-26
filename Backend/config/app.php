70a275bf2a485fd74e8a3d7d3502c6b4c1ac2782
Madserker <madserker@gmail.com>
2018-10-26 23:01:03 +0200

    fixed import jwt for laravel 5.5

--- Backend/config/app.php
+++ Backend/config/app.php
@@ -150,7 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         /*
          * Application Service Providers...
          */
