b062335f568f86b123daad2afc100dc64ec05caf
Sergi Hurtado <madserker@gmail.com>
2018-11-09 00:21:12 +0100

    delete comment button and styles

--- Frontend/src/app/comment/comment.component.ts
+++ Frontend/src/app/comment/comment.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Comment } from '../_models/Comment.interface';
+import { User } from '../_models/User.interface';
 
 @Component({
   selector: 'app-comment',
@@ -9,6 +10,7 @@ import { Comment } from '../_models/Comment.interface';
 export class CommentComponent implements OnInit {
 
   @Input() comment : Comment;
+  @Input() currentUser : User;
 
   constructor() { }
 
