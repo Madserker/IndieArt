5079cc063786b250a491bb64951570529500e2de
Sergi Hurtado <madserker@gmail.com>
2018-11-17 18:14:47 +0100

    followers frontEnd

--- Frontend/src/app/profile/profile-view-image/profile-view-image.component.ts
+++ Frontend/src/app/profile/profile-view-image/profile-view-image.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from '../../_models/User.interface';
+import { UsersService } from '../../users.service';
 
 @Component({
   selector: 'app-profile-view-image',
@@ -10,11 +11,21 @@ export class ProfileViewImageComponent implements OnInit {
 
   @Input() user: User;
   @Input() currentUser: User;
+
+  followers : User [];
+  following : User [];
   
-  constructor() { }
+  constructor(private usersService : UsersService) { }
 
   ngOnInit() {
     console.log(this.currentUser)
+
+  }
+
+  ngOnChanges(){
+    this.usersService.getFollowers(this.user.username).subscribe(result => {
+      this.followers = result as User []
+      })
   }
 
 }
