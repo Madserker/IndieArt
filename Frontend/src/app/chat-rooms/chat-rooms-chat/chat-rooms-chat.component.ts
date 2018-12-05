058b5c6e79f2864e84e172860200302b61f4e566
Sergi Hurtado <madserker@gmail.com>
2018-12-05 12:20:58 +0100

    chat background image is team image

--- Frontend/src/app/chat-rooms/chat-rooms-chat/chat-rooms-chat.component.ts
+++ Frontend/src/app/chat-rooms/chat-rooms-chat/chat-rooms-chat.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Chat } from '../../_models/Chat.interface';
+import { ListsService } from '../../lists.service';
 
 @Component({
   selector: 'app-chat-rooms-chat',
   templateUrl: './chat-rooms-chat.component.html',
-  styleUrls: ['./chat-rooms-chat.component.less']
+  styleUrls: ['./chat-rooms-chat.component.less'],
+  providers: [ListsService]
 })
 export class ChatRoomsChatComponent implements OnInit {
   
   @Input() chat : Chat;
-  image = '../../../../Backend/storage/app/profileImages/jwxfyxNJNE9zo7dUkVLEvrb8UouIQdbOT9Bbxzgs.jpeg';
-  constructor() { }
+  image = '';
+  constructor(private lists : ListsService) { }
 
   ngOnInit() {
+
+  }
+  ngOnChanges(){
+    this.lists.getTeamByUsername(this.chat.name).subscribe(
+      result=>this.image=result.profile_picture
+    );
   }
 
 }
